49f03758c5765c9dac48e0ce49d8a38acfa2f748
erikaosgue <1476@holbertonschool.com>
2021-04-13 01:48:07 -0500

    Create the chats activity

--- app/src/main/java/com/erikaosgue/mychatapp/activities/ChatActivity.kt
+++ app/src/main/java/com/erikaosgue/mychatapp/activities/ChatActivity.kt
@@ -3,8 +3,15 @@ package com.erikaosgue.mychatapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.erikaosgue.mychatapp.databinding.ActivityChatBinding
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
 
 class ChatActivity : AppCompatActivity() {
+
+    var userId: String?=null
+    var mFirebaseDatabaseRef: DatabaseReference?= null
+    var mFirebaseUser: FirebaseUser?=null
+
 
     lateinit var actChatBinding: ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
